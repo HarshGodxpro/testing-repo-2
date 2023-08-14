21a68f38bbefd36f4b03566fba833b9f84c27c7c
HARSH RAJ <119205864+HarshGodxpro@users.noreply.github.com>
2023-08-14 21:13:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -99,6 +99,6 @@ LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 #Direct Link Generator By @Developerr_Bots
-DIRECT_GEN_DB = int(environ.get("DIRECT_GEN_DB", "-1001906907851")) # Enter your channel id
-DIRECT_GEN_URL = environ.get("DIRECT_GEN_URL", "https://developerr-bots.xyz/") # https://example.com/
+DIRECT_GEN_DB = int(environ.get("DIRECT_GEN_DB", "-1001908991684")) # Enter your channel id
+DIRECT_GEN_URL = environ.get("DIRECT_GEN_URL", "https://profiletolink78-c13c753c3752.herokuapp.com/") # https://example.com/
 DIRECT_GEN = bool(DIRECT_GEN_DB and DIRECT_GEN_URL)
