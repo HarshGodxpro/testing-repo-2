7b3fa1671a78b81a9381c7ee97e3606464b5d337
HARSH RAJ <119205864+HarshGodxpro@users.noreply.github.com>
2023-09-25 15:26:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/spidybhau/YDTest.git /spidyauto
+  git clone https://github.com/HarshGodxpro/testing-repo-2.git /spidyauto
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /spidyauto
