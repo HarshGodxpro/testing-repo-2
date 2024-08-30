db10a27d50452359c75d33e2db0bdf13b397e22f
HARSH RAJ <119205864+HarshGodxpro@users.noreply.github.com>
2024-08-30 15:35:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -100,5 +100,5 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 #Direct Link Generator By @Developerr_Bots
 DIRECT_GEN_DB = int(environ.get("DIRECT_GEN_DB", "-1002073550724")) # Enter your channel id
-DIRECT_GEN_URL = environ.get("DIRECT_GEN_URL", "https://fmfiletolink100-8c324379edb6.herokuapp.com/") # https://example.com/
+DIRECT_GEN_URL = environ.get("DIRECT_GEN_URL", "https://fmfiletolink-32f172629478.herokuapp.com/") # https://example.com/
 DIRECT_GEN = bool(DIRECT_GEN_DB and DIRECT_GEN_URL)
